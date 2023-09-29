50d73aa22a91d0f1b32f8e1c9cd549dfdbaa4b08
Michael-IvyTech-CS <mbarthauer@ivytech.edu>
2023-09-29 18:53:45 -0400

    Added more data

--- src/maintenance_entry.py
+++ src/maintenance_entry.py
@@ -9,8 +9,18 @@ Description:
 
 class MaintenanceEntry:
     """Stores information relating to a specific maintenance check."""
-    def __init__(self, vehicle, date, odometer=0):
+
+    def __init__(self, vehicle, date, signature, costs, cost_labels, odometer=0):
+        """Initializes MaintenanceEntry"""
         self.vehicle = vehicle  # Vehicle object reference
         self.date = date  # "YYYY-MM-DD" iso format, date maintenance occurred
-        self.miles_as_of = odometer
+        self.miles_as_of = odometer  # if not specified, this is set the same as vehicle's current odometer
+        self.check_signed_by = signature  # who signed the check / paid for the maintenance
+
+        # the following parallel tuples could be put in a dictionary instead
+        self.check_entries = tuple(costs)  # tuple of costs enumerated in the bill
+        # parallel tuple to check_entries, stores string labels for each cost ("parts", "labor", etc)
+        self.check_categories = tuple(cost_labels)
+
+
         pass
--- src/vehicle.py
+++ src/vehicle.py
@@ -10,4 +10,5 @@ Description: Defines class Vehicle which stores vehicle information.
 class Vehicle:
     miles_total = 0  # total number of miles
     miles_log = {}  # {"YYYY-MM-DD": <miles_driven>} for each day the vehicle is driven
+    maintenance_log = []  # list of MaintenanceEntry instances
     pass
