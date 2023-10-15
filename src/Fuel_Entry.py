bf83f3688b402166208f6ab1799b8264cf31ac47
pchitwood2 <142836574+pchitwood2@users.noreply.github.com>
2023-10-15 00:11:50 -0400

    Update Fuel_Entry.py

--- src/Fuel_Entry.py
+++ src/Fuel_Entry.py
@@ -1,17 +1,20 @@
 class fuelEntry:
+    # Initializes Fuel Entry Class
     def __init__(self, fuelLogDate, gallons, totalCost, odometer_Reading):
-        self.fuelLogDate = fuelLogDate    
-        self.gallons = gallons
-        self.totalCost = totalCost
-        self.odometer_Reading = odometer_Reading
+        self.fuelLogDate = fuelLogDate    # The date of the fuel entry
+        self.gallons = gallons    # Amount of gallons input
+        self.totalCost = totalCost    # Total cost of fuel
+        self.odometer_Reading = odometer_Reading    # Odometer Reading at purchase of fuel
        
        
-        
+    # How Fuel Entry Object is represented as a string    
     def __repr__(self):
         return f"Log: {(fuelEntry_list.index(self)+1)} \nGallons of Fuel: {self.gallons}\nTotal Cost of Fuel: {self.totalCost}\nOdometer Reading: {self.odometer_Reading}\n\n"
-    
+
+# List where Fuel Entry objects are stored
 fuelEntry_list = []
 
+# Function that initializes and adds newly created Fuel Entry objects to list
 def create_FuelEntry_List(FuelEntry):
     fuelEntry_desc_list = [FuelEntry]
     fuelEntry_list.append(FuelEntry)
